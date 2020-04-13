785e0cbd10cf39154b3e8a79cb2df09557ab0823
karthiksekark <karthiksekark@gmail.com>
2020-04-13 13:31:08 +0530

    Updates

---
+++ precache-manifest.d9d6c392345fd24fd73864360eca565b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2afdf65bd7c27992d0d5107c1153fc4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d3c5157766b34fbefd2",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "40a0b17adfe80b238185",
+    "url": "/static/js/2.52291a8e.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/static/js/2.52291a8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d3c5157766b34fbefd2",
+    "url": "/static/js/main.ef8c4943.chunk.js"
+  },
+  {
+    "revision": "c8f25319213e36dbe0ed",
+    "url": "/static/js/runtime-main.4b97932c.js"
+  }
+]);
